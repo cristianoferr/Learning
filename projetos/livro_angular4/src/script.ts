ae043e8dc16bcaa637cb3a93dd1f959965971dd0
Cristiano Ferreira <cristianorj@gmail.com>
2017-05-24 16:50:26 -0300

    continuando a criar tutoriais de angular

---
+++ projetos/livro_angular4/src/script.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "joke"
+    template:`
+    <h1>What did the cheese say when it look in the mirror?</h1>
+    <p>Halloumi(hello me)</p>`
+})
+
+class JokeComponent{
+
+}
