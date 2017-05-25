372a03c4212095f425d2e35e7f206e956520864b
Cristiano Ferreira <cristianorj@gmail.com>
2017-05-25 17:22:02 -0300

    ...

--- projetos/angular2-fundamentals/src/app/app.module.ts
+++ projetos/angular2-fundamentals/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
