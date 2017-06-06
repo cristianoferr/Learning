71caeb221268b3bbecde8e85d8222b3314532c1b
Cristiano <cristianorj@gmail.com>
2017-06-05 23:42:22 -0300

    ...

--- projetos/curso-comple-guide/my-dream-app/src/app/cockpit/cockpit.component.ts
+++ projetos/curso-comple-guide/my-dream-app/src/app/cockpit/cockpit.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = "";
+  //newServerName = "";
   newServerContent = "";
 
 
@@ -17,12 +17,13 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
   }
 
 
